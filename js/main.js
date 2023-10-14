4f3d837165ec47469b0a120e18f2bd8f04ea737e
UNAINR <munain605@gmail.com>
2023-10-13 22:58:20 -0700

    portfolio

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+} 
+
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+  var typed = new Typed('#element', {
+      strings: ['<i>Web developer </i>','<i>UI/UX Desginer </i>','<i>Responsive Design </i>'],
+      typeSpeed: 35,
+      loop: true,
+      backSpeed: 11,
+      smartBackspace: true,
+    
+    });
+
+
