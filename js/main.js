1cd0d31273ac741ec19ea8f18e201db7726e1eab
UNAINR <munain605@gmail.com>
2024-03-28 18:40:13 +0500

    file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Header Scroll
 let nav = document.querySelector(".navbar");
 window.onscroll = function () {
-  if (document.documentElement.scrollTop > 20) {
+  if (document.documentElement.scrollTop > 140) {
     nav.classList.add("header-scrolled");
   } else {
     nav.classList.remove("header-scrolled");
@@ -26,4 +26,43 @@ var typed = new Typed("#element", {
   loop: true,
   backSpeed: 11,
   smartBackspace: true,
+});
+
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem("darkMode");
+
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add("darkmode");
+  // 2. Update darkMode in localStorage
+  localStorage.setItem("darkMode", "enabled");
+};
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove("darkmode");
+  // 2. Update darkMode in localStorage
+  localStorage.setItem("darkMode", null);
+};
+
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === "enabled") {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener("click", () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem("darkMode");
+
+  // if it not current enabled, enable it
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+    // if it has been enabled, turn it off
+  } else {
+    disableDarkMode();
+  }
 });
